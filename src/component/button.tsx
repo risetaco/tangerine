e93eb48b6cc775ad07b69e10b31a017f2d199a67
Condro Wiyono <condro@outlook.co.id>
2024-06-22 12:18:27 +0700

    chore: init library

---
+++ src/component/button.tsx
@@ -0,0 +1,10 @@
+import { ButtonHTMLAttributes } from "react";
+
+const Button = ({
+  children,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return <button {...props}>{children}</button>;
+};
+
+export default Button;
