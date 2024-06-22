42d18346d4aff7c61c445299962182cbb8f8d628
Condro Wiyono <condro@outlook.co.id>
2024-06-22 12:53:44 +0700

    feat: add tailwind and change build to vite library

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,10 +1,11 @@
 import { ButtonHTMLAttributes } from "react";
 
-const Button = ({
-  children,
-  ...props
-}: ButtonHTMLAttributes<HTMLButtonElement>) => {
-  return <button {...props}>{children}</button>;
+const Button = ({ children, ...props }: ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button className="min-w-16 py-2 px-4 text-white rounded bg-primary" {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
